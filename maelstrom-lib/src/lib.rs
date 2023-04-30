5bf460916a10152cb9e38a45aa4f5be1b8fad641
Raj Vengalil <avranju@nerdworks.dev>
2023-04-30 21:23:02 +0530

    Node gains some basic functionality

--- maelstrom-lib/src/lib.rs
+++ maelstrom-lib/src/lib.rs
@@ -1,16 +1,25 @@
-use std::{collections::HashMap, future::Future, pin::Pin};
+use std::{
+    cell::RefCell, collections::HashMap, future::Future, io::Error as IoError, pin::Pin, rc::Rc,
+};
 
 use async_trait::async_trait;
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
 use thiserror::Error;
+use tokio::io::{self, AsyncBufReadExt, BufReader};
 
 #[derive(Error, Debug)]
 pub enum Error {
+    #[error("I/O error")]
+    Io(#[from] IoError),
+
+    #[error("JSON serialisation error")]
+    Json(#[from] serde_json::Error),
+
     #[error("Unknown error")]
     Unknown,
 }
 
-#[derive(Serialize, Deserialize)]
+#[derive(Serialize, Deserialize, Clone)]
 pub struct Body<T> {
     #[serde(rename = "type")]
     pub type_: String,
@@ -21,15 +30,40 @@ pub struct Body<T> {
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct Message<T> {
+pub struct Reply<T> {
+    pub in_reply_to: u64,
+
+    #[serde(flatten, skip_serializing_if = "Option::is_none")]
+    pub body: Option<T>,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Message<T>
+where
+    T: Clone,
+{
     pub src: String,
     pub dest: String,
     pub body: Body<T>,
 }
 
+#[derive(Clone)]
+pub struct Context<B> {
+    node: Node<B>,
+}
+
+impl<B> Context<B>
+where
+    B: Serialize + DeserializeOwned + Send + Clone,
+{
+    pub async fn reply_to<R: Serialize>(&self, src: Message<B>, body: R) -> Result<(), Error> {
+        self.node.reply_to(src, body).await
+    }
+}
+
 #[async_trait]
-pub trait MessageHandler<Body: Serialize + DeserializeOwned + Send> {
-    async fn handle(&self, msg: Message<Body>) -> Result<(), Error>;
+pub trait MessageHandler<Body: Serialize + DeserializeOwned + Send + Clone> {
+    async fn handle(&self, ctx: Context<Body>, msg: Message<Body>) -> Result<(), Error>;
 }
 
 /// Blanket impl of `MessageHandler` for all functions where signature
@@ -37,43 +71,77 @@ pub trait MessageHandler<Body: Serialize + DeserializeOwned + Send> {
 impl<F, B, U> MessageHandler<B> for F
 where
     U: Future<Output = Result<(), Error>> + Send + 'static,
-    F: Fn(Message<B>) -> U,
-    B: Serialize + DeserializeOwned + Send,
+    F: Fn(Context<B>, Message<B>) -> U,
+    B: Serialize + DeserializeOwned + Send + Clone,
 {
     fn handle<'life0, 'async_trait>(
         &'life0 self,
+        ctx: Context<B>,
         msg: Message<B>,
     ) -> Pin<Box<dyn Future<Output = Result<(), Error>> + Send + 'async_trait>>
     where
         'life0: 'async_trait,
         Self: 'async_trait,
     {
-        Box::pin(self(msg))
+        Box::pin(self(ctx, msg))
     }
 }
 
 pub struct Node<B> {
-    handlers: HashMap<String, Box<dyn MessageHandler<B>>>,
+    handlers: Rc<RefCell<HashMap<String, Box<dyn MessageHandler<B>>>>>,
+}
+
+impl<B> Clone for Node<B> {
+    fn clone(&self) -> Self {
+        Self {
+            handlers: self.handlers.clone(),
+        }
+    }
 }
 
 impl<B> Node<B>
 where
-    B: Serialize + DeserializeOwned + Send,
+    B: Serialize + DeserializeOwned + Send + Clone,
 {
     pub fn new() -> Self {
         Node {
-            handlers: HashMap::new(),
+            handlers: Rc::new(RefCell::new(HashMap::new())),
         }
     }
 
     pub fn handle<T: MessageHandler<B> + 'static>(&mut self, type_: &str, handler: T) {
-        self.handlers.insert(type_.to_string(), Box::new(handler));
+        self.handlers
+            .borrow_mut()
+            .insert(type_.to_string(), Box::new(handler));
+    }
+
+    pub async fn reply_to<R: Serialize>(&self, src: Message<B>, body: R) -> Result<(), Error> {
+        Ok(())
+    }
+
+    pub async fn run(self) -> Result<(), Error> {
+        // each message is sent to us as a single line
+        let stdin = io::stdin();
+        let reader = BufReader::new(stdin);
+        let mut lines = reader.lines();
+        let ctx = Context { node: self.clone() };
+
+        while let Some(line) = lines.next_line().await? {
+            let msg: Message<B> = serde_json::from_str(&line)?;
+            let handlers = self.handlers.borrow();
+
+            let f = handlers
+                .iter()
+                .map(|(_, h)| h.handle(ctx.clone(), msg.clone()));
+        }
+
+        Ok(())
     }
 }
 
 impl<B> Default for Node<B>
 where
-    B: Serialize + DeserializeOwned + Send,
+    B: Serialize + DeserializeOwned + Send + Clone,
 {
     fn default() -> Self {
         Self::new()
