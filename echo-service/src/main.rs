560a05ffd30483dad1b6b394ca682ace6082ab0e
Raj Vengalil <avranju@nerdworks.dev>
2023-05-01 19:56:51 +0530

    Some refactoring. Echo service done.

--- echo-service/src/main.rs
+++ echo-service/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use log::trace;
+use maelstrom_lib::{Context, Error, Message, Node};
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    env_logger::init();
+
+    let mut node = Node::new().await;
+    node.handle("echo", handle_echo).await;
+    node.run().await?;
+
+    Ok(())
+}
+
+#[derive(Serialize, Deserialize)]
+struct Echo {
+    echo: String,
+}
+
+async fn handle_echo(ctx: Context, msg: Message) -> Result<(), Error> {
+    if let Some(echo) = msg.body.parse::<Echo>()? {
+        trace!("Received echo: {}", echo.echo);
+
+        ctx.reply_to_with(msg, "echo_ok".to_string(), Some(echo))
+            .await?;
+    }
+
+    Ok(())
 }
--- maelstrom-lib/src/lib.rs
+++ maelstrom-lib/src/lib.rs
@@ -1,12 +1,15 @@
-use std::{
-    cell::RefCell, collections::HashMap, future::Future, io::Error as IoError, pin::Pin, rc::Rc,
-};
+use std::{collections::HashMap, future::Future, io::Error as IoError, pin::Pin, sync::Arc};
 
 use async_trait::async_trait;
 use futures::future::join_all;
+use log::trace;
 use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use serde_json::Value as JsonValue;
 use thiserror::Error;
-use tokio::io::{self, AsyncBufReadExt, BufReader};
+use tokio::{
+    io::{self, AsyncBufReadExt, BufReader},
+    sync::{mpsc, Mutex},
+};
 
 #[derive(Error, Debug)]
 pub enum Error {
@@ -16,72 +19,92 @@ pub enum Error {
     #[error("JSON serialisation error")]
     Json(#[from] serde_json::Error),
 
+    #[error("Reply channel send error")]
+    ReplySendError(#[from] mpsc::error::SendError<(Message, String, Option<JsonValue>)>),
+
     #[error("Unknown error")]
     Unknown,
 }
 
-#[derive(Serialize, Deserialize, Clone)]
-pub struct Body<T> {
+#[derive(Serialize, Deserialize)]
+pub struct Reply<B> {
+    pub in_reply_to: u64,
+
+    #[serde(flatten, skip_serializing_if = "Option::is_none")]
+    pub body: Option<B>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Body {
     #[serde(rename = "type")]
     pub type_: String,
     pub msg_id: u64,
 
     #[serde(flatten, skip_serializing_if = "Option::is_none")]
-    pub body: Option<T>,
+    pub body: Option<JsonValue>,
 }
 
-#[derive(Serialize, Deserialize)]
-pub struct Reply<T> {
-    pub in_reply_to: u64,
-
-    #[serde(flatten, skip_serializing_if = "Option::is_none")]
-    pub body: Option<T>,
+impl Body {
+    pub fn parse<B: DeserializeOwned>(&self) -> Result<Option<B>, Error> {
+        Ok(self
+            .body
+            .as_ref()
+            .map(|json| serde_json::from_value(json.clone()))
+            .transpose()?)
+    }
 }
 
-#[derive(Serialize, Deserialize, Clone)]
-pub struct Message<T>
-where
-    T: Clone,
-{
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct Message {
     pub src: String,
     pub dest: String,
-    pub body: Body<T>,
+    pub body: Body,
 }
 
 #[derive(Clone)]
-pub struct Context<B> {
-    node: Node<B>,
+pub struct Context {
+    node: Node,
+    reply_queue_tx: mpsc::Sender<(Message, String, Option<JsonValue>)>,
 }
 
-impl<B> Context<B>
-where
-    B: Serialize + DeserializeOwned + Send + Clone,
-{
-    pub async fn reply_to<R: Serialize>(&self, src: Message<B>, body: R) -> Result<(), Error> {
-        self.node.reply_to(src, body).await
+impl Context {
+    pub async fn reply_to(&self, src: Message, type_: String) -> Result<(), Error> {
+        Ok(self.reply_queue_tx.send((src, type_, None)).await?)
+    }
+
+    pub async fn reply_to_with<R: Serialize>(
+        &self,
+        src: Message,
+        type_: String,
+        body: Option<R>,
+    ) -> Result<(), Error> {
+        Ok(self
+            .reply_queue_tx
+            .send((
+                src,
+                type_,
+                body.map(|b| serde_json::to_value(b)).transpose()?,
+            ))
+            .await?)
     }
 }
 
 #[async_trait]
-pub trait MessageHandler<Body>
-where
-    Body: Serialize + DeserializeOwned + Send + Clone,
-{
-    async fn handle(&self, ctx: Context<Body>, msg: Message<Body>) -> Result<(), Error>;
+pub trait MessageHandler {
+    async fn handle(&self, ctx: Context, msg: Message) -> Result<(), Error>;
 }
 
 /// Blanket impl of `MessageHandler` for all functions where signature
 /// matches `MessageHandler::handle`.
-impl<F, B, U> MessageHandler<B> for F
+impl<F, U> MessageHandler for F
 where
     U: Future<Output = Result<(), Error>> + Send + 'static,
-    F: Fn(Context<B>, Message<B>) -> U,
-    B: Serialize + DeserializeOwned + Send + Clone,
+    F: Fn(Context, Message) -> U,
 {
     fn handle<'life0, 'async_trait>(
         &'life0 self,
-        ctx: Context<B>,
-        msg: Message<B>,
+        ctx: Context,
+        msg: Message,
     ) -> Pin<Box<dyn Future<Output = Result<(), Error>> + Send + 'async_trait>>
     where
         'life0: 'async_trait,
@@ -91,35 +114,110 @@ where
     }
 }
 
-pub struct Node<B> {
-    handlers: Rc<RefCell<HashMap<String, Box<dyn MessageHandler<B>>>>>,
+#[derive(Serialize, Deserialize)]
+pub struct Init {
+    node_id: String,
+    node_ids: Vec<String>,
 }
 
-impl<B> Clone for Node<B> {
-    fn clone(&self) -> Self {
-        Self {
-            handlers: self.handlers.clone(),
-        }
+async fn handle_init(ctx: Context, msg: Message) -> Result<(), Error> {
+    if let Some(init) = msg.body.parse::<Init>()? {
+        trace!("Received init message for node {}.", init.node_id);
+
+        let mut state = ctx.node.state.lock().await;
+        state.id = init.node_id;
+        state.node_ids = init.node_ids;
+
+        ctx.reply_to(msg, "init_ok".to_string()).await?;
+    }
+
+    Ok(())
+}
+
+#[derive(Clone)]
+pub struct Node {
+    #[allow(clippy::type_complexity)]
+    handlers: Arc<Mutex<HashMap<String, Vec<Box<dyn MessageHandler + Send>>>>>,
+    state: Arc<Mutex<State>>,
+}
+
+impl Node {
+    pub async fn id(&self) -> String {
+        self.state.lock().await.id.clone()
+    }
+
+    pub async fn node_ids(&self) -> Vec<String> {
+        self.state.lock().await.node_ids.clone()
     }
 }
 
-impl<B> Node<B>
-where
-    B: Serialize + DeserializeOwned + Send + Clone,
-{
-    pub fn new() -> Self {
-        Node {
-            handlers: Rc::new(RefCell::new(HashMap::new())),
+struct State {
+    id: String,
+    node_ids: Vec<String>,
+    msg_id_counter: usize,
+    reply_queue_tx: mpsc::Sender<(Message, String, Option<JsonValue>)>,
+    reply_queue_rx: Option<mpsc::Receiver<(Message, String, Option<JsonValue>)>>,
+}
+
+impl Node {
+    pub async fn new() -> Self {
+        let (reply_queue_tx, reply_queue_rx) = mpsc::channel(256);
+
+        let mut node = Node {
+            handlers: Arc::new(Mutex::new(HashMap::new())),
+            state: Arc::new(Mutex::new(State {
+                id: String::new(),
+                node_ids: vec![],
+                msg_id_counter: 1,
+                reply_queue_tx,
+                reply_queue_rx: Some(reply_queue_rx),
+            })),
+        };
+
+        node.handle("init", handle_init).await;
+
+        node
+    }
+
+    pub async fn handle<T: MessageHandler + Send + 'static>(&mut self, type_: &str, handler: T) {
+        let handler = Box::new(handler);
+        let handlers = &mut self.handlers.lock().await;
+
+        if let Some(handlers) = handlers.get_mut(type_) {
+            handlers.push(handler);
+        } else {
+            handlers.insert(type_.to_string(), vec![handler]);
         }
     }
 
-    pub fn handle<T: MessageHandler<B> + 'static>(&mut self, type_: &str, handler: T) {
-        self.handlers
-            .borrow_mut()
-            .insert(type_.to_string(), Box::new(handler));
-    }
+    pub async fn reply_to(
+        &self,
+        src: Message,
+        type_: String,
+        body: Option<JsonValue>,
+    ) -> Result<(), Error> {
+        let mut state = self.state.lock().await;
 
-    pub async fn reply_to<R: Serialize>(&self, src: Message<B>, body: R) -> Result<(), Error> {
+        let msg_id = state.msg_id_counter as u64;
+        state.msg_id_counter += 1;
+
+        let reply = Reply {
+            in_reply_to: src.body.msg_id,
+            body,
+        };
+
+        let msg = Message {
+            src: state.id.clone(),
+            dest: src.src,
+            body: Body {
+                msg_id,
+                type_,
+                body: Some(serde_json::to_value(reply)?),
+            },
+        };
+
+        println!("{}", serde_json::to_string(&msg)?);
+
         Ok(())
     }
 
@@ -128,29 +226,34 @@ where
         let stdin = io::stdin();
         let reader = BufReader::new(stdin);
         let mut lines = reader.lines();
-        let ctx = Context { node: self.clone() };
+        let ctx = Context {
+            node: self.clone(),
+            reply_queue_tx: self.state.lock().await.reply_queue_tx.clone(),
+        };
+        let id = self.id().await;
+        let mut reply_queue_rx = {
+            self.state
+                .lock()
+                .await
+                .reply_queue_rx
+                .take()
+                .expect("Reply queue receiver must have been set")
+        };
 
-        while let Some(line) = lines.next_line().await? {
-            let msg: Message<B> = serde_json::from_str(&line)?;
+        loop {
+            tokio::select! {
+                Some((msg, type_, body)) = reply_queue_rx.recv() => {
+                    self.reply_to(msg, type_, body).await?;
+                },
 
-            let _ = join_all(
-                self.handlers
-                    .borrow()
-                    .iter()
-                    .map(|(_, h)| h.handle(ctx.clone(), msg.clone())),
-            )
-            .await;
+                Ok(Some(line)) = lines.next_line() => {
+                    trace!("[{}] Received: {}", id, line);
+                    let msg: Message = serde_json::from_str(&line)?;
+                    if let Some(handlers) = self.handlers.lock().await.get(&msg.body.type_) {
+                        let _ = join_all(handlers.iter().map(|h| h.handle(ctx.clone(), msg.clone()))).await;
+                    }
+                },
+            }
         }
-
-        Ok(())
-    }
-}
-
-impl<B> Default for Node<B>
-where
-    B: Serialize + DeserializeOwned + Send + Clone,
-{
-    fn default() -> Self {
-        Self::new()
     }
 }
